f18c1dde13671f31c976980168ddd61cb50f88e0
njnusrat32856 <nusratjahannaima32856@gmail.com>
2024-11-07 00:05:41 +0600

    add

--- fintech_mobile_app/lib/pages/transfer_money.dart
+++ fintech_mobile_app/lib/pages/transfer_money.dart
@@ -1,3 +1,4 @@
+import 'package:fintech_mobile_app/widgets/credit_card.dart';
 import 'package:flutter/material.dart';
 
 class TransferMoney extends StatelessWidget {
@@ -12,6 +13,59 @@ class TransferMoney extends StatelessWidget {
             icon: Icon(Icons.arrow_back_ios_new)
         ),
         title: Text("Transfer"),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Chosse Cards',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(height: 12,),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(3, (index) => Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: CreditCard(),
+                    ))
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 25,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Chosse Recipients',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(height: 25,),
+                Container(),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                      children: List.generate(3, (index) => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                        child: CreditCard(),
+                      ))
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
